8575c55054d9d66b44b60c9661255b98c968fe32
Justin A. Williams <justinawcs@gmail.com>
2019-05-21 19:25:13 -0400

    Change select function, start a basic style

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-board = document.getElementById('board');
+board = {};
+
 
 function draw_board(columns, rows) {
   var col, row;
-  var i = 0;
+  //var i = 0;
   for(row=1; row<=rows; row++){
     for(col=1; col<=columns; col++){
-      item = document.createElement("BUTTON");
-      item.id = "item-"+i;
-      item.className = "item";
+      var item = document.createElement("BUTTON");
+      item.id = "item-"+col+"-"+row;
+      item.className = "space";
       item.type = "button";
       item.onclick = function(){select(this.id)};
-      item.innerHTML = i;
+      item.innerHTML = col +"-"+row;
+      item.setAttribute("data-col", col);
+      item.setAttribute("data-row", row);
       item.setAttribute("data-space", "");
       document.getElementById("board").appendChild(item);
-      i++;
+      //i++;
     }
   }
 }
 
-function select(item){
-  console.log(item);
-}
+function select(item, color){
+  console.log("clicked "+item);
+  var col = document.getElementById(item).getAttribute("data-col");
+  var row = document.getElementById(item).getAttribute("data-row");
+  console.log(col + " " + row);
 
-// returns only the number part of the item name, 'item-3' -> 3
-function strip_name(item){
-  var start = item.indexOf("-") + 1;
-  return Number( item.slice(start));
 }
